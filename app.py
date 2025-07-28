811dd95fca7af2a208ca6b840d3287c071a42117
Vishal Sharma <vishalsharma07ms@gmail.com>
2025-07-29 00:29:42 +0530

    fourth commit

--- app.py
+++ app.py
@@ -162,4 +162,4 @@ def send_to_crm(name, phone, appointment_time):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT")))
